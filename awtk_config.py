96d0a2fb84b2296ab295c44495a7f4b546fa91c7
lixianjing <xianjimli@hotmail.com>
2019-06-21 17:32:33 -0700

    fix build scripts

--- awtk_config.py
+++ awtk_config.py
@@ -50,9 +50,9 @@ TOOLS_PREFIX='/opt/28x/gcc-4.4.4-glibc-2.11.1-multilib-1.0/arm-fsl-linux-gnueabi
 #TOOLS_PREFIX='/opt/poky/1.7/sysroots/x86_64-pokysdk-linux/usr/bin/arm-poky-linux-gnueabi/arm-poky-linux-gnueabi-'
 
 #for pc build
-TOOLS_PREFIX=''
-TSLIB_LIB_DIR=''
-OS_FLAGS='-g -Wall '
+#TOOLS_PREFIX=''
+#TSLIB_LIB_DIR=''
+#OS_FLAGS='-g -Wall '
 
 OS_LIBS = OS_LIBS + ['stdc++', 'pthread', 'm', 'dl']
 COMMON_CCFLAGS = COMMON_CCFLAGS + ' -DLINUX -DHAS_PTHREAD -DENABLE_CURSOR '
