5b0d12a6f9200ed1713469f4a72fcc92f1358dcd
lixianjing <xianjimli@hotmail.com>
2024-12-03 18:34:50 +0800

    improve release.sh

--- release.sh
+++ release.sh
@@ -2,8 +2,15 @@
 
 #example: ./release.sh ../awtk-examples/HelloWorld-Demo exename
 
+# 在应用程序目录下运行:
+# ~/work/awtk-root/awtk-linux-fb/release.sh . demo
+
+AWTK_LINUX_FB_DIR="$(dirname "${BASH_SOURCE[0]}")"
+AWTK_LINUX_FB_DIR="$(cd "$AWTK_LINUX_FB_DIR" && pwd)"
+AWTK_DIR=$AWTK_LINUX_FB_DIR"/../awtk"
+
 EXE_NAME=demoui
-APP_ROOT="../awtk"
+APP_ROOT=$AWTK_DIR
 
 if [ $1 ]; then
   if [ ! -d $1 ]; then
@@ -21,6 +28,7 @@ echo "EXE_NAME = ${EXE_NAME}"
 echo "APP_ROOT = ${APP_ROOT}" 
 
 rm -rf release release.tar.gz
-python3 ../awtk/scripts/release.py ${EXE_NAME} ${APP_ROOT}
-cp -r config release/config
+python3 $AWTK_DIR/scripts/release.py ${EXE_NAME} ${APP_ROOT}
+cp -rvf $AWTK_LINUX_FB_DIR/config release/config
+
 tar -czf release.tar.gz release/
