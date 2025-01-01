ae46d4dc78647ccb07ed2f26f4606e4ad4f525e8
lixianjing <xianjimli@hotmail.com>
2025-01-01 18:24:57 +0800

    update copyright date

--- awtk-port/input_thread/common_coord.h
+++ awtk-port/input_thread/common_coord.h
@@ -3,7 +3,7 @@
  * Author: AWTK Develop Team
  * Brief:  公共坐标
  *
- * Copyright (c) 2018 - 2023  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ * Copyright (c) 2018 - 2025 Guangzhou ZHIYUAN Electronics Co.,Ltd.
  *
  * this program is distributed in the hope that it will be useful,
  * but without any warranty; without even the implied warranty of
